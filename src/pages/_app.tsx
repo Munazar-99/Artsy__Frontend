fb7983a45ddf9afabd6fd044931e4f80bb25f5fa
Munazar Ali <munazar.ali.001@gmail.com>
2023-08-20 17:45:51 +0200

    configure google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import Header from '@/components/Header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import{ Analytics } from '@vercel/analytics/react';
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,11 +11,19 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC}`}></script>
+        <script
+          dangerouslySetInnerHTML = {{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC});`
+          }}/>
       </Head>
       <Header />
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] ">
-        <Component {...pageProps}/>
-        <Analytics />
+        <Component {...pageProps} />
       </main>
     </>
   )
